80660a333103c9b416832ddcc943ce4b7723af04
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-06-01 13:44:45 +0300

    array comparison

---
+++ src/week5/ArrayComparison.java
@@ -0,0 +1,22 @@
+package week5;
+
+public class ArrayComparison {
+
+    public void compare(int[] arr1, int[] arr2) {
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.print(arr1[i] + ", ");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayComparison arrayComparison = new ArrayComparison();
+
+        int[] arr1 = {3, 2, 8, 5, 10};
+        int[] arr2 = {9, 20, 18, 5, 17, 3};
+        arrayComparison.compare(arr1, arr2);
+    }
+}

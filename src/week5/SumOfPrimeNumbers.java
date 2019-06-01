854049fe65472dc9893b6cb153e7a7820aa9a63c
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-06-01 10:30:49 +0300

    sum of primes

---
+++ src/week5/SumOfPrimeNumbers.java
@@ -0,0 +1,45 @@
+package week5;
+
+import java.util.Scanner;
+
+public class SumOfPrimeNumbers {
+
+    public boolean isPrime(int num) {
+        int counter = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                counter++;
+            }
+        }
+
+        if (counter == 2) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int sumPrimes(int limit) {
+        int sum = 0;
+        for (int i = 2; i <= limit; i++) {
+            boolean prime = isPrime(i);
+            if (prime) {
+                sum = sum + i;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        SumOfPrimeNumbers sumOfPrimeNumbers = new SumOfPrimeNumbers();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is the limit? : ");
+        int limit = scanner.nextInt();
+
+        int result = sumOfPrimeNumbers.sumPrimes(limit);
+
+        System.out.println("Result : " + result);
+    }
+}

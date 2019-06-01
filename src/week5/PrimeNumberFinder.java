3a573af714c3a1e4248ff97f63dfea3fd766abae
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-06-01 09:48:58 +0300

    christmas tree

---
+++ src/week5/PrimeNumberFinder.java
@@ -0,0 +1,29 @@
+package week5;
+
+public class PrimeNumberFinder {
+
+    public boolean isPrime(int num) {
+        int counter = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                counter++;
+            }
+        }
+
+        if (counter == 2) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        PrimeNumberFinder primeNumberFinder = new PrimeNumberFinder();
+        int number = 39;
+        boolean prime = primeNumberFinder.isPrime(number);
+
+        System.out.println(number + " is prime? " + prime);
+    }
+
+}

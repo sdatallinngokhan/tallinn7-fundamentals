bba9c13d6fe3449cb2da2b1543bd8830056fd9b6
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-06-02 09:37:53 +0300

    even characters of a string

---
+++ src/week5/EvenCharactersOfAString.java
@@ -0,0 +1,20 @@
+package week5;
+
+import java.util.Scanner;
+
+public class EvenCharactersOfAString {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Your word : ");
+        String word = scanner.nextLine();
+
+        char[] wordArray = word.toCharArray();
+
+        for (int i = 0; i < wordArray.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(wordArray[i]);
+            }
+        }
+    }
+}
